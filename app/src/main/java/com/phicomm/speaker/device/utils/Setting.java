81029aa7d6961555f4601c4546e5afec68d6d6af
ivan <xj.zhang@msn.com>
2025-05-03 08:28:51 +0800

    enable debug logs

--- app/src/main/java/com/phicomm/speaker/device/utils/Setting.java
+++ app/src/main/java/com/phicomm/speaker/device/utils/Setting.java
@@ -1,8 +1,8 @@
 package com.phicomm.speaker.device.utils;
 
 public class Setting {
-    public static boolean ASR_OPT_PRINT_LOG = false;
-    public static boolean ASR_OPT_PRINT_ENGINE_LOG = false;
+    public static boolean ASR_OPT_PRINT_LOG = true;
+    public static boolean ASR_OPT_PRINT_ENGINE_LOG = true;
     public static boolean ASR_OPT_PRINT_TIME_LOG = false;
 
     public static int LogMgrLevel = 1;
